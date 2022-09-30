18298c4b5519166b6f04111182c441950c589280
DeenoDev <deyanoklaasen@outlook.com>
2022-09-30 08:06:20 +0200

    completed password validation using javascript.

--- main.js
+++ main.js
@@ -8,8 +8,29 @@ const inputs = [email,phone,password,confirmPassword,errorMessage];
 
 inputs.forEach((item)=>{
     item.addEventListener("focusin", ()=> {
-        errorMessage.textContent = '';
+        errorMessage.textContent = " ";
         item.classList.remove("error");
-        
+        if(item==password || item==confirmPassword){
+            password.classList.remove("error");
+        }
     });
 });
+
+const submit = (e) => {
+    e.preventDefault();
+    if (password.value !== confirmPassword.value){
+        password.classList.add('error');
+        confirmPassword.classList.add('error');
+        errorMessage.textContent = "Passwords don't match";
+        return;
+    }
+    if(password.value.length < 8){
+        password.classList.add('error');
+        errorMessage.textContent = "Password needs to be at least 8 characters long.";
+        return;
+    }
+    if(!password.value.match())
+};
+
+const form = document.querySelector("form");
+form.addEventListener("submit", submit);
